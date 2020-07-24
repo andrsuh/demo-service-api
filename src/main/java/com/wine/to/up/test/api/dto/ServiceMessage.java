bc007d390171941e293d38aa2361af6f3130fed8
Ivan Zelenkov <vertvery@gmail.com>
2020-07-24 18:02:47 +0300

    add new method with dto

--- src/main/java/com/wine/to/up/test/api/dto/ServiceMessage.java
+++ src/main/java/com/wine/to/up/test/api/dto/ServiceMessage.java
@@ -1,8 +1,16 @@
 package com.wine.to.up.test.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import java.util.Map;
+
 @Data
+@AllArgsConstructor
 public class ServiceMessage {
+    @NotNull
+    private Map<String, byte[]> headers;
+    @NotNull
     private String message;
 }
--- src/main/java/com/wine/to/up/test/api/feign/KafkaServiceClient.java
+++ src/main/java/com/wine/to/up/test/api/feign/KafkaServiceClient.java
@@ -1,5 +1,6 @@
 package com.wine.to.up.test.api.feign;
 
+import com.wine.to.up.test.api.dto.ServiceMessage;
 import com.wine.to.up.test.api.service.KafkaService;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Primary;
@@ -13,4 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface KafkaServiceClient extends KafkaService {
     @PostMapping(value = "/send/{topicName}")
     void sendMessage(@PathVariable String topicName, @RequestParam String message);
+
+    @PostMapping(value = "/send/{topicName}")
+    void sendMessageWithHeaders(@PathVariable String topicName, @RequestParam ServiceMessage message);
 }
