16f2f94f7ecaa7947e8a2d5c320468150ab869ef
Ivan Zelenkov <vertvery@gmail.com>
2020-07-25 16:05:40 +0300

    add method to service

--- src/main/java/com/wine/to/up/test/api/service/KafkaService.java
+++ src/main/java/com/wine/to/up/test/api/service/KafkaService.java
@@ -1,5 +1,8 @@
 package com.wine.to.up.test.api.service;
 
+import com.wine.to.up.test.api.dto.ServiceMessage;
+
 public interface KafkaService {
     void sendMessage(String topicName, String message);
+    void sendMessageWithHeaders( String topicName,  ServiceMessage message);
 }
